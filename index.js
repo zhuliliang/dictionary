5e1f66cc609e34f4bac5757ba8208ef793f46796
Zhu Liliang <liliang.zhu@gmail.com>
2018-02-01 17:01:29 +0800

    2 basic services

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var cors = require('cors')
+
+var serveStatic = require('serve-static');
+
 var app = express();
+app.use(cors())
 //数据库mongodb使用
 var mongoosejs = require('./db/mongoose');
 mongoosejs.start(); //配置
@@ -9,25 +14,62 @@ var mongoose = require('mongoose');
 var Trans = mongoose.model('Trans');
 
 //某一个接口实现，包含数据库操作
-app.get('/search/:key', function(req, res, next) {
+app.get('/api/search/:key', function(req, res, next) {
 
   var _key = req.params.key;
   var cond = {
     key: {
-      "$all": ["definition", _key]
+      "$all": [decodeURIComponent(_key)]
     }
   };
+  var projection = { _id: 0, key: 1, uri: 1, tmp:1};
 
-  var trans = new Trans();
-  Trans.find(cond, function(error, result) {
+  Trans.find(cond, projection, function(error, result) {
     if (!error) {
-      res.json(result);
+      //console.log(result);
+      newresult = result.map(record => {
+        // var newRecord = Object.assign({}, record, {cn: record.tmp[0], en: record.tmp[1]});
+        var newRecord = record._doc;
+        newRecord.cn = record.tmp[0];
+        newRecord.en = record.tmp[1];
+        return newRecord;
+      })
+      //console.log(newresult);
+      res.json(newresult);
     } else {
       console.log(error);
     }
   });
 });
 
-app.listen(3000, function() {
-  console.log("Server Running on http://localhost:3000");
+//某一个接口实现，包含数据库操作
+app.get('/api/keywords', function(req, res, next) {
+
+  var cond = { };
+
+  var projection = { _id: 0, key: 1 };
+
+  var trans = new Trans();
+  Trans.find(cond, projection, function(error, result) {
+    if (!error) {
+      var r = [];
+      result.map((x) => {
+        x['key'].map((y) => {
+          r.push(y);
+        })
+        return r;
+      })
+      res.json(Array.from(new Set(r)));
+    } else {
+      console.log(error);
+    }
+  });
+});
+
+
+app.use('/', serveStatic(__dirname + '/_site'));
+
+
+app.listen(30003, function() {
+  console.log("Server Running on http://localhost:30003");
 });
---
+++ init/keywords.js
@@ -0,0 +1,4 @@
+//引入mongoose模块
+var mongoose = require('mongoose');
+//引入上面实现的config.js
+var config = require('./config');
